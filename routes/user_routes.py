3ffb7de9874c65559166b467716e8b0d08ca66c8
Manvanth M <4mh23cs083@gmail.com>
2026-02-07 14:49:37 +0530

    me

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from db import get_db
 from sqlalchemy.orm import Session
 from repositories.User_repo import UserRepo
@@ -17,10 +17,13 @@ router = APIRouter()
     
 @router.post("/signup")
 def signup(user: UserSchema, db: Session = Depends(get_db)):
-    user_repo = UserRepo(db)
+    User_repo = UserRepo(db)
+    existing_user = User_repo.get_user_by_email(user.email)
+    if existing_user:
+        raise HTTPException(status_code=400, detail="User already exists")
     # Convert Pydantic schema to SQLAlchemy model
     db_user = User(email=user.email, password=user.password)
-    user_repo.add_user(db_user)
+    User_repo.add_user(db_user)
     return {"message": "User signed up successfully"}
     
 
